42277c3ba64a783c88dcc75eef86b54a5efd32c8
prakash <patel.prakash.er@gmail.com>
2021-04-19 12:25:06 +0530

    first

---
+++ assignment1.js
@@ -0,0 +1,71 @@
+//1st
+var s1 = "$120"
+var l1 = s1.length
+var s2 = s1.slice(1,l1)
+if(parseInt(s2)==120){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+
+
+
+
+
+//2nd
+
+var  arr = ["jazz","blues"]
+arr.push("Rock-n-Roll")
+var l = arr.length/2
+
+l = Math.floor(l)
+
+arr[l] = "k"
+arr.splice(0, 0, "Rap");
+arr.splice(1, 0, "Reggae ");
+console.log(arr)
+
+
+// let user = {
+//     name: 'John',
+//     age: 30
+//   };
+//   var c = 0
+//   const count = (user)=> {
+//     //write your code here
+//     console.log(c+1)
+   
+//     } // 2
+// count()
+
+// var s = "1234555"
+// c  = 0
+// for(var x in s){
+//     c+=parseInt(s[x])
+ 
+
+// }
+// console.log(c)
+
+// var d = {name:"prakash",lname:"patel"}
+// var y = 0
+// var numbers = [45, 4, 9, 16, 25]
+// numbers.forEach(function(numbers1){
+//     console.log(numbers1)})
+
+
+var a = []
+a.push["hello"]
+if(a.length!=0){
+    localStorage.setItem(a)
+}
+else{
+    console.log("buyy")
+}
+
+
+
+
+
+
